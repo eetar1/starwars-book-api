677faea8014e59bf25f0ee5e7d6a0c65350e76a8
eetar1 <ekwan14@gmail.com>
2021-09-20 13:22:00 -0700

    clean up and add spotless

--- src/main/java/com/starwars/api/StarwarsBookApplication.java
+++ src/main/java/com/starwars/api/StarwarsBookApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StarwarsBookApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StarwarsBookApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(StarwarsBookApplication.class, args);
+    }
 }
